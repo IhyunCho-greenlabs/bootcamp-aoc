f5ea97b86eff3c4351f89d63d7439ab4db9b44ae
조이현 <ihyuncho@Ihyun-makbook.local>
2022-09-14 16:54:08 +0900

    part2 in progress

--- clojure/src/aoc2020_8.clj
+++ clojure/src/aoc2020_8.clj
@@ -20,21 +20,21 @@
   {:line 0 :acc 0 :visited #{} :record input})
 
 (defn parse-file-to-record
-  "parse file input to data record
+  "parse file input to data record which including initialization
    input: string file
    output: boot codes vector
+   {:line 0 :acc 0 :visited #{} 
    ({:op \"nop\", :value 0}
    {:op \"acc\", :value 1}
    {:op \"jmp\", :value 4}..
    "
   [input]
   (let [result (->>  input
-         io/resource
-         slurp
-         string/split-lines
-         (map make-bootcode-map))]
-    {:line 0 :acc 0 :visited #{} :record result}
-    ))
+                     io/resource
+                     slurp
+                     string/split-lines
+                     (map make-bootcode-map))]
+    {:line 0 :acc 0 :visited #{} :record result}))
 
 
 (defn execute-operation
@@ -54,7 +54,7 @@
      ({:op \"nop\", :value 0}
      {:op \"acc\", :value 1}...
    "
-  [program] 
+  [program]
   (let [{:keys [line acc visited record]} program
         code (nth record line)
         op (:op code)
@@ -79,4 +79,61 @@
        ;aggregate
        last
        :acc))
+
+
+(if (= 4 5) 1 3)
+
+;part 2
+
+
+
+(defn test-fixing-one-instruction
+  [input]
+  (let [program {:line 0 :acc 0 :visited #{} :record input}
+        last-line (dec (count input))
+        result (->> program
+                    (iterate execute-operation)
+                    (take-while #(or (not-contains? (:visited %) (:line %)) (= (:line %) last-line)))
+                    last)]
+    (if (= (:line result) last-line)
+      (:acc result)
+      nil)))
+
+(defn parse-file-to-vec-record
+  "parse file input to data record
+   input: string file
+   output: boot codes vector
+   ({:op \"nop\", :value 0}
+   {:op \"acc\", :value 1}
+   {:op \"jmp\", :value 4}..
+   "
+  [input]
+  (->>  input
+        io/resource
+        slurp
+        string/split-lines
+        (map make-bootcode-map)
+        vec))
+
+(defn make-test-codes
+  [input]
+  (for [fix-target (range 9)
+        :let [fix-target-code (nth input fix-target)]
+        :when (and (not= (:op fix-target-code) "acc")
+                   (not (and (= (:op fix-target-code) "nop") (= (:value fix-target-code) 0))))] ;never change nop 0 code
+    (update-in input [fix-target :op]
+               #(if (= "nop" %) "jmp" "nop"))))
+
+
+(comment
+  (->> "2020_8_sample.txt"
+       ;parse
+       parse-file-to-vec-record
+       make-test-codes
+
+       (drop-while #(nil? (test-fixing-one-instruction %)))
+      ;(map test-fixing-one-instruction )
+       )
+       ;process
+  )
 
